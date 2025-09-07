d9917fb097f3424a32a83b660ab6407c8dd53199
Yash Bansal <demonstrikk@gmail.com>
2025-09-07 12:05:27 +0530

    minor fix

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(cors());
 // OR restrict to frontend URL only:
 app.use(
   cors({
-    origin: ["https://localhost:5173", "https://clonytweet-three.vercel.app"],
+    origin: ["http://localhost:5173", "https://clonytweet-three.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
